eae8b7510df7106b79645bfb7c82cd0913001434
Sotoarmando <armando29@live.com>
2021-10-06 01:00:50 -0500

    Approach: follow design guidelines

--- src/components/Columnmenu.js
+++ src/components/Columnmenu.js
@@ -21,12 +21,17 @@ export default function Colummenu() {
   return [
     <div key="Colummenu0" className=" row  space_between ">{menubtn}</div>,
     <div key="Colummenu1" className={`mobilehide col nav_col_0 bodyheight ${active ? 'active' : ''} p corebox_x14 back_2 border_r3 `}>
+
       <div className="row space_between">
         {menubtn}
         <span role="button" aria-hidden="true" onClick={() => setActive(!active)} className="corebox_2 center maxedcorebox_x4">O/C</span>
       </div>
-      <div className="corebox_11 col center items_start pad_l30 back_2 bodyheight">
-        {
+      <div className="col space_between allsize">
+        <div className="f_5 ffam_opensans f700 pad_l30">
+          Vespa
+        </div>
+        <div className="corebox_11 col center items_start pad_l30 back_2 ">
+          {
             paths.map(([k, v]) => (
               [
                 <input key={`Columenupaths${k}`} type="radio" id={k} name="Colummenupaths" value={k} className="" />,
@@ -36,8 +41,17 @@ export default function Colummenu() {
               ]
             ))
         }
+        </div>
+        <div className="f_5 ffam_opensans f700 ">
+          <div className="row center corebox_7">
+            {['twitter', 'facebook', 'google', 'instagram', 'path'].map((e) => (
+              <span key={e} className="corebox_2 maxedcorebox_x2 center  borderradius_22 half_horizontalmar mar_l25 mar_r25">
+                <span className={`maskicon_${e} back_black_o2 iconsize_23 maxedcorebox_x2 `} />
+              </span>
+            ))}
+          </div>
+        </div>
       </div>
-
     </div>,
     <div key="Colummenu2" className={`desktophide col nav_col_0d ${active ? 'active' : ''} p allsize back_2 border_r3 `}>
       <div className="row space_between">
